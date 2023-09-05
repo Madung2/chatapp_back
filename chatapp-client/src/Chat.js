ade731ea9e69438ff7a98b0939bd7c40aa7c27cc
LAPTOP-EIABQG0H\tulip <tasha@xquant-ai.com>
2023-09-05 18:44:33 +0900

    fix: change backend socket url

--- chatapp-client/src/Chat.js
+++ chatapp-client/src/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000',{
+const socket = io('http://13.210.66.89:3000',{
     transports: ['websocket']
 });
 
